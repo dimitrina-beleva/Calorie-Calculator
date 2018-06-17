1843dc3c9245053fea8a247520e80203d4d205ef
Dimitrina <d_d_b@mail.bg>
2018-06-17 18:13:01 +0300

    Added Go to top button with function, table with foods and drinks and info on Recommentations html file

--- SS_Dimitrina/js/main.js
+++ SS_Dimitrina/js/main.js
@@ -14,6 +14,25 @@ function openTab(tabName,elmnt,color) {
     elmnt.style.backgroundColor = color;
     
 }
+
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myButton").style.display = "block";
+    } else {
+        document.getElementById("myButton").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
