8a04cde3b5066344363e5630d2ed447ea9cedc69
Dimitrina <d_d_b@mail.bg>
2018-07-10 22:39:24 +0300

    Made validation and reformatting

--- SS_Dimitrina/js/recipes.js
+++ SS_Dimitrina/js/recipes.js
@@ -1,5 +1,6 @@
 openTab('recipes', document.getElementById("recipes-button"), 'rgb(252, 202, 137)');
 
+//Shows and hides the recipes
 function showRecipe(recipeToBeShown) {
     var recipe = $('#' + recipeToBeShown);
     var isVisible = recipe.is(":visible");
