fbbc0daa84fd8c0d31427d640924f3328d6e49f6
Dimitrina <d_d_b@mail.bg>
2018-07-10 22:14:33 +0300

    Fixed show/hide method

--- SS_Dimitrina/js/recipes.js
+++ SS_Dimitrina/js/recipes.js
@@ -1,14 +1,13 @@
 openTab('recipes', document.getElementById("recipes-button"), 'rgb(252, 202, 137)');
 
-//TODO
-$(function showRecipe(recipeToBeShown) {
+function showRecipe(recipeToBeShown) {
     var recipe = $('#' + recipeToBeShown);
     var isVisible = recipe.is(":visible");
     if (isVisible) {
-    recipe.css('visibility','hidden');
-    recipe.hide();
+        recipe.css('visibility','hidden');
+        recipe.hide();
     } else {
-    recipe.css('visibility','visible');
-    recipe.show();
+        recipe.css('visibility','visible');
+        recipe.show();
     }
-});
+}
